d8271ba5670482d72e1bef992ada5bd7b8232478
boshuua <113433127+boshuua@users.noreply.github.com>
2024-09-26 15:28:54 +0100

    Create Math.cpp

---
+++ Math.cpp
@@ -0,0 +1,52 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+
+
+int main() 
+{
+    // Initial Code - 1st time 
+    int num1; // Declaring DataType 
+    int num2;
+    
+    std::cout << "Enter in the first number: "; // Asking user for input 
+    std::cin >> num1;
+    
+    std::cout << "Enter in the second number: ";
+    std::cin >> num2;
+    
+    int product = num1 * num2;
+    int sum = num1 + num 2
+    
+    std::cout << sum;
+    std::cout << product;
+    return 0;
+}
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    
+    std::cout << "Enter in the first number: "; // Asking user for input 
+    std::cin >> num1;
+    
+    std::cout << "Enter in the second number: ";
+    std::cin >> num2;
+    
+    std::cout << "Sum: " << num1 + num2 << "\n";
+    std::cout << "Product: " << num1 * num2;
+    if (num1 == num2)
+    {
+        std::cout << "Numbers are the same"
+    }
+    else if (num1 > num2)
+    {
+        std::cout << "Num1 is greater than num2"; 
+    }
+    else
+    {
+        std::cout <<  "Num2 is less than num2"
+    }
+    return 0;
+}
