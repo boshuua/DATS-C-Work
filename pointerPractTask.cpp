912a913a05829032282682915b0391be577bf4c4
boshuua <113433127+boshuua@users.noreply.github.com>
2024-10-17 10:03:34 +0100

    Update pointerPractTask.cpp

--- pointerPractTask.cpp
+++ pointerPractTask.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-
-int main()
+void swap(int* ptrnum, int* ptrnum2)
 {
-	int ptrnum = 7;
-
-	int* ptr = &ptrnum;
-
-	std::cout << "Value of the var: " << ptrnum << std::endl;
-	std::cout << "Memory Address in Pointer: " << ptr << std::endl;
-	std::cout << "Pointer points to value: " << *ptr << std::endl;
+    int temp = *ptrnum; // * = value of pointer
+    
+    *ptrnum = *ptrnum2;
+    
+    *ptrnum2 = temp;
+}
+int main() {
+    int ptrnum = 7, ptrnum2 = 12;
+    
+    std::cout << "Before Swapping x = " << ptrnum << ", y = " << ptrnum2 << std::endl;
+    
+    swap(&ptrnum, &ptrnum2);
+    
+    std::cout << "After Swapping x = " << ptrnum << ", y = " << ptrnum2 << std::endl;
 }
